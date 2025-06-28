d18e27dc93d057cdf6b74729b5bb296960229cdb
Djboy08 <justindoane@gmail.com>
2025-06-28 19:32:08 -0400

    log

--- lib/datastore.ts
+++ lib/datastore.ts
@@ -1,5 +1,5 @@
 export async function opencloud_ordered_datastore_get(datatstore_name: string) {
-  console.log("datastore fetched");
+  console.log("datastore fetched: ", datatstore_name);
   const v1 = `v1/universes/1180269832/orderedDataStores/${datatstore_name}/scopes/global/entries`;
   const paramters = `max_page_size=100&order_by=desc`;
   const url = `https://apis.roblox.com/ordered-data-stores/${v1}?${paramters}`;
